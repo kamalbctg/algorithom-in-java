661af75ff8325f4bf36b4ba1af7fa10b209df277
Kamal Hossain <kamalbctg@gmail.com>
2020-06-17 08:54:22 +0600

    improved version

--- src/main/java/org/algorithom/MoveElementToEnd/Solution.java
+++ src/main/java/org/algorithom/MoveElementToEnd/Solution.java
@@ -3,16 +3,13 @@ package org.algorithom.MoveElementToEnd;
 import java.util.List;
 
 public class Solution {
+    //T: O(n), S: O(1)
     public static List<Integer> moveElementToEnd(List<Integer> array, int target) {
         int left = 0;
         int right = array.size() - 1;
         while (left < right) {
-            if (array.get(left) != target) {
-                left++;
-                continue;
-            }
-            while (left < right && array.get(right) == target) right--;
             if (array.get(left) == target) {
+                while (left < right && array.get(right) == target) right--;
                 int tmp = array.get(left);
                 array.set(left, array.get(right));
                 array.set(right, tmp);
