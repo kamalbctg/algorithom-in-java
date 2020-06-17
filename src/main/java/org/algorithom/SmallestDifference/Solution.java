c58a89b1d601070cfe6ca6296febe7a60054caf9
Kamal Hossain <kamalbctg@gmail.com>
2020-06-17 09:33:20 +0600

    update

--- src/main/java/org/algorithom/SmallestDifference/Solution.java
+++ src/main/java/org/algorithom/SmallestDifference/Solution.java
@@ -4,37 +4,32 @@ import java.util.Arrays;
 
 public class Solution {
 
-    //T: O(nlon(n)+mlog(m))  S: O(1)
+
     public static int[] smallestDifference(int[] arrayOne, int[] arrayTwo) {
         int[] pair = new int[2];
+        int left = 0;
+        int right = 0;
+        int diff = Integer.MAX_VALUE;
         int smallest = Integer.MAX_VALUE;
-        int difference = Integer.MAX_VALUE;
-
         Arrays.sort(arrayOne);
         Arrays.sort(arrayTwo);
-
-        int firstArrayIdx = 0;
-        int secondArrayIdx = 0;
-
-        while (firstArrayIdx < arrayOne.length && secondArrayIdx < arrayTwo.length) {
-            var firstNum = arrayOne[firstArrayIdx];
-            var secondNum = arrayTwo[secondArrayIdx];
-
-            if (firstNum < secondNum) {
-                difference = secondNum - firstNum;
-                firstArrayIdx++;
-            } else if (firstNum > secondNum) {
-                difference = firstNum - secondNum;
-                secondArrayIdx++;
+        while (left < arrayOne.length && right < arrayTwo.length) {
+            int x = arrayOne[left];
+            int y = arrayTwo[right];
+            if (x < y) {
+                diff = y - x;
+                left++;
+            } else if (x > y) {
+                diff = x - y;
+                right++;
             } else {
-                return new int[]{firstNum, secondNum};
+                return new int[]{x, y};
             }
 
-            if (smallest > difference) {
-                smallest = difference;
-                pair = new int[]{firstNum, secondNum};
+            if (smallest > diff) {
+                smallest = diff;
+                pair = new int[]{x, y};
             }
-
         }
         return pair;
     }
