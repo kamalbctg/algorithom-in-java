1c40ed1a584c1ad84e8f5098b5923644cbac9a5a
Kamal Hossain <kamalbctg@gmail.com>
2020-06-20 11:36:20 +0600

    Write a function that takes in an array of integers and, without sorting the   input array, returns a sorted array of the three largest integers in the input   array.
      The function should return duplicate integers if necessary; for example, it
      should return `[10, 10, 12]` for an input array of `[10, 5, 9, 10, 12]`
    
     #####Sample Input
        array  = [141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]
    #####Sample Output
        [18, 141, 541]

--- src/main/java/org/algorithom/FindThreeLargestNumbers/Solution.java
+++ src/main/java/org/algorithom/FindThreeLargestNumbers/Solution.java
@@ -1,29 +1,31 @@
 package org.algorithom.FindThreeLargestNumbers;
 
+import static java.lang.Integer.MIN_VALUE;
+
 public class Solution {
     //Time: O(n)
     //Space: O(1)
     public static int[] findThreeLargestNumbers(int[] array) {
-        int[] numbers = new int[]{Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE};
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > numbers[2]) {
-                shift(2, array[i], numbers);
-            } else if (array[i] > numbers[1]) {
-                shift(1, array[i], numbers);
-            } else if (array[i] > numbers[0]) {
-                shift(0, array[i], numbers);
-            }
+        int[] numbers = new int[]{MIN_VALUE, MIN_VALUE, MIN_VALUE};
+        for (int num : array) {
+            updateNumbersArray(num, numbers);
         }
         return numbers;
     }
 
-    private static void shift(int position, int num, int[] numbers) {
-        for (int i = 0; i <= position; i++) {
-            if (i == position) {
-                numbers[i] = num;
-            } else {
-                numbers[i] = numbers[i + 1];
-            }
+    private static void updateNumbersArray(int num, int[] numbers) {
+        if (num > numbers[2])
+            shift(2, num, numbers);
+        else if (num > numbers[1])
+            shift(1, num, numbers);
+        else if (num > numbers[0])
+            shift(0, num, numbers);
+    }
+
+    private static void shift(int index, int num, int[] numbers) {
+        for (int i = 0; i < index; i++) {
+            numbers[i] = numbers[i + 1];
         }
+        numbers[index] = num;
     }
 }
--- src/test/java/org/algorithom/FindThreeLargestNumbers/SolutionTest.java
+++ src/test/java/org/algorithom/FindThreeLargestNumbers/SolutionTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.*;
 public class SolutionTest {
     @Test
     public void findThreeLargestNumbers() {
+        Assert.assertArrayEquals( new int[]{10, 10, 12}, Solution.findThreeLargestNumbers(new int[]{10, 5, 9, 10, 12}));
         Assert.assertArrayEquals( new int[]{18, 141, 541}, Solution.findThreeLargestNumbers(new int[]{141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7}));
         Assert.assertArrayEquals( new int[]{7, 8, 55}, Solution.findThreeLargestNumbers(new int[]{55, 7, 8}));
         Assert.assertArrayEquals( new int[]{-2, -1, 7}, Solution.findThreeLargestNumbers(new int[]{-1, -2, -3, -7, -17, -27, -18, -541, -8, -7, 7}));
