68916bcf0d3115048b8dfff0120f5822824516db
Kamal Hossain <kamalbctg@gmail.com>
2020-06-20 09:18:43 +0600

    Write a function that takes in an array of integers and returns a sorted   version of that array. Use the Selection Sort algorithm to sort the array.
     #####Sample Input
        array = [8, 5, 2, 9, 5, 6, 3]
    #####Sample Output
        [2, 3, 5, 5, 6, 8, 9]

--- src/main/java/org/algorithom/SelectionSort/Solution.java
+++ src/main/java/org/algorithom/SelectionSort/Solution.java
@@ -4,18 +4,16 @@ public class Solution {
     //Time:  Best: O(n), Avg: (n^2),  Worst:O(n^2)
     //Space: O(1)
     public static int[] selectionSort(int[] array) {
-        int selectedIdx = 0;
-        while (selectedIdx < array.length) {
-            int smallestIdx = selectedIdx;
-            for (int i = selectedIdx; i < array.length; i++) {
-                if (array[i] < array[smallestIdx]) {
-                    smallestIdx = i;
+        for (int i = 0; i < array.length; i++) {
+            int smallest = i;
+            for (int j = smallest + 1; j < array.length; j++) {
+                if (array[j] < array[smallest]) {
+                    smallest = j;
                 }
             }
-            int tmp = array[selectedIdx];
-            array[selectedIdx] = array[smallestIdx];
-            array[smallestIdx] = tmp;
-            selectedIdx++;
+            int tmp = array[i];
+            array[i] = array[smallest];
+            array[smallest] = tmp;
         }
         return array;
     }
