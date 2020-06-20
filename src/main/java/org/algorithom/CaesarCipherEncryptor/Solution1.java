d4e7757439b3f35bcb4f58af6e1122d6c418e2a4
Kamal Hossain <kamalbctg@gmail.com>
2020-06-20 09:56:13 +0600

    Given a non-empty string of lowercase letters and a non-negative integer representing a key,  write a function that returns a new string obtained by shifting every letter in the input string  by k positions in the alphabet, where k is the key.
     Note that letters should "wrap" around the alphabet; in other words, the letter `z` shifted by one
     returns the letter `a`.
    
     #####Sample Input
        string = "xyz"
        key    = 2
    #####Sample Output
        "zab"

--- src/main/java/org/algorithom/CaesarCipherEncryptor/Solution1.java
+++ src/main/java/org/algorithom/CaesarCipherEncryptor/Solution1.java
@@ -2,15 +2,15 @@ package org.algorithom.CaesarCipherEncryptor;
 
 public class Solution1 {
     public static String caesarCypherEncryptor(String str, int key) {
-        char[] encryptChars = new char[str.length()];
+        char[] encryptedChar = new char[str.length()];
         for (int i = 0; i < str.length(); i++) {
-            int encryptCharCode = str.charAt(i) + (key % 26); //wrap key between 26
-            if (encryptCharCode <= 122) {
-                encryptChars[i] = (char) encryptCharCode;
-            } else {
-                encryptChars[i] = (char) (96 + encryptCharCode % 122); // wrap between a-z or 97 - 122
+            int encryptedCharCode = str.charAt(i) + (key % 26);
+            if (encryptedCharCode <= 122)
+                encryptedChar[i] = (char) encryptedCharCode;
+            else {
+                encryptedChar[i] = (char) (96 + encryptedCharCode % 122);
             }
         }
-        return new String(encryptChars);
+        return new String(encryptedChar);
     }
 }
--- src/main/java/org/algorithom/CaesarCipherEncryptor/Solution2.java
+++ src/main/java/org/algorithom/CaesarCipherEncryptor/Solution2.java
@@ -4,15 +4,15 @@ public class Solution2 {
     // T: O(n) S: O(n)
     public static String caesarCypherEncryptor(String str, int key) {
         String alphabets = "abcdefghijklmnopqrstuvwxyz";
-        char[] encryptChars = new char[str.length()];
+        char[] encryptedChar = new char[str.length()];
         for (int i = 0; i < str.length(); i++) {
-            int encryptCharCode = alphabets.indexOf(str.charAt(i)) + (key % 26); //wrap key between 0-25
-            if (encryptCharCode <= 25) {
-                encryptChars[i] =  alphabets.charAt(encryptCharCode);
-            } else {
-                encryptChars[i] = alphabets.charAt(-1 + encryptCharCode % 25); // wrap between a-z or 0 - 25
+            int encryptedCharCode = alphabets.indexOf(str.charAt(i)) + (key % 26);
+            if (encryptedCharCode <= 25)
+                encryptedChar[i] = alphabets.charAt(encryptedCharCode);
+            else {
+                encryptedChar[i] =alphabets.charAt(-1 + encryptedCharCode % 25);
             }
         }
-        return new String(encryptChars);
+        return new String(encryptedChar);
     }
 }
