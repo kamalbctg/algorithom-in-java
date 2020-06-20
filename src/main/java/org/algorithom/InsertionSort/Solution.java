df46f38f738b9fc6002d5e5fa9eeb3e5b33e24c0
Kamal Hossain <kamalbctg@gmail.com>
2020-06-20 09:04:25 +0600

    Write a function that takes in an array of integers and returns a sorted   version of that array. Use the Insertion Sort algorithm to sort the array.
     #####Sample Input
        array = [8, 5, 2, 9, 5, 6, 3]
    #####Sample Output
        [2, 3, 5, 5, 6, 8, 9]

--- src/main/java/org/algorithom/InsertionSort/Solution.java
+++ src/main/java/org/algorithom/InsertionSort/Solution.java
@@ -4,6 +4,7 @@ public class Solution {
     //Time:  Best: O(n), Avg: (n^2),  Worst:O(n^2)
     //Space: O(1)
     public static int[] insertionSort(int[] array) {
+
         for (int i = 1; i < array.length; i++) {
             int j = i;
             while (j > 0 && array[j] < array[j - 1]) {
