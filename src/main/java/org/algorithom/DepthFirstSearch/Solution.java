b0b01e8c9bbc56e4c99aff040ffda70f3d811de1
Kamal Hossain <kamalbctg@gmail.com>
2020-06-21 13:22:45 +0600

    Update

---
+++ src/main/java/org/algorithom/DepthFirstSearch/Solution.java
@@ -0,0 +1,29 @@
+package org.algorithom.DepthFirstSearch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    static class Node {
+        String name;
+        List<Node> children = new ArrayList<>();
+
+        public Node(String name) {
+            this.name = name;
+        }
+
+        public List<String> depthFirstSearch(List<String> array) {
+            array.add(name);
+            for (int i = 0; i < children.size(); i++) {
+                children.get(i).depthFirstSearch(array);
+            }
+            return array;
+        }
+
+        public Node addChild(String name) {
+            Node child = new Node(name);
+            children.add(child);
+            return this;
+        }
+    }
+}
