52fd237f2a271765667117072183a934579e6662
Kamal Hossain <kamalbctg@gmail.com>
2020-06-21 15:09:52 +0600

    update

--- src/main/java/org/algorithom/ValidateBST/Solution.java
+++ src/main/java/org/algorithom/ValidateBST/Solution.java
@@ -6,18 +6,12 @@ public class Solution {
     public static boolean validateBst(BST tree) {
         return validateBstHelper(tree, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
+
     //O(n) time, O(d) space
     private static boolean validateBstHelper(BST tree, int minValue, int maxValue) {
-        if (tree.value < minValue || tree.value >= maxValue) {
-            return false;
-        }
-        if (tree.left != null && !validateBstHelper(tree.left, minValue, tree.value)) {
-            return false;
-        }
-
-        if (tree.right != null && !validateBstHelper(tree.right, tree.value, maxValue)) {
-            return false;
-        }
+        if (tree.value < minValue || tree.value >= maxValue) return false;
+        if (!validateBstHelper(tree.left, minValue, maxValue)) return false;
+        if (!validateBstHelper(tree.right, tree.value, maxValue)) return false;
         return true;
     }
 }
