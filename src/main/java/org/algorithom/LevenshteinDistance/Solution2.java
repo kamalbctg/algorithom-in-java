2d3905c53ae49acf6d6addbc72cecdc7c8f67f1f
Kamal Hossain <kamalbctg@gmail.com>
2020-06-22 14:00:09 +0600

    update

--- src/main/java/org/algorithom/LevenshteinDistance/Solution2.java
+++ src/main/java/org/algorithom/LevenshteinDistance/Solution2.java
@@ -5,16 +5,16 @@ public class Solution2 {
         String small = (str1.length() < str2.length()) ? str1 : str2;
         String big = (str1.length() >= str2.length()) ? str1 : str2;
 
-        int[] evenEditDistances = new int[small.length() + 1];
-        int[] oddEditDistances = new int[small.length() + 1];
+        int[] evenEditDistances = new int[big.length() + 1];
+        int[] oddEditDistances = new int[big.length() + 1];
 
-        for (int i = 0; i < small.length() + 1; i++) {
-            oddEditDistances[i] = i;
+        for (int i = 0; i < big.length() + 1; i++) {
+            evenEditDistances[i] = i;
         }
 
         int[] currentEdits;
         int[] previousEdits;
-        for (int i = 1; i < big.length() + 1; i++) {
+        for (int i = 1; i < small.length() + 1; i++) {
             if (i % 2 == 0) {
                 currentEdits = evenEditDistances;
                 previousEdits = oddEditDistances;
@@ -23,15 +23,15 @@ public class Solution2 {
                 previousEdits = evenEditDistances;
             }
             currentEdits[0] = i;
-            for (int j = 1; j < small.length() + 1; j++) {
-                if (big.charAt(i - 1) == small.charAt(j - 1)) {
+            for (int j = 1; j < big.length() + 1; j++) {
+                if (small.charAt(i - 1) == big.charAt(j - 1)) {
                     currentEdits[j] = previousEdits[j - 1];
                 } else {
                     currentEdits[j] = 1 + min(previousEdits[j - 1], previousEdits[j], currentEdits[j - 1]);
                 }
             }
         }
-        return big.length() % 2 == 0 ? evenEditDistances[small.length()] : oddEditDistances[small.length()];
+        return small.length() % 2 == 0 ? evenEditDistances[big.length()] : oddEditDistances[big.length()];
     }
 
     private static int min(int i, int j, int k) {
