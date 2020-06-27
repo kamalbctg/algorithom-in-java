1cb3225e7b96d1c672722a5d6be32c5cecc43689
kamal <kamalbctg@gmail.com>
2020-06-27 22:19:45 +0600

    updates

--- src/main/java/org/algorithom/GroupAnagrams/Solution.java
+++ src/main/java/org/algorithom/GroupAnagrams/Solution.java
@@ -1,9 +1,40 @@
 package org.algorithom.GroupAnagrams;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Solution {
     public static List<List<String>> groupAnagrams(List<String> words) {
-        return null;
+        if (words.size() == 0) return new ArrayList<>();
+
+        List<String> sortedWords = new ArrayList<>();
+        for (String word : words) {
+            char[] charArray = word.toCharArray();
+            Arrays.sort(charArray);
+            sortedWords.add(new String(charArray));
+        }
+
+        List<Integer> indices = IntStream.range(0, words.size()).boxed().collect(Collectors.toList());
+        indices.sort((a, b) -> sortedWords.get(a).compareTo(sortedWords.get(b)));
+
+        List<List<String>> groupedAnagrams = new ArrayList<>();
+        List<String> anagrams = new ArrayList<>();
+        String group = sortedWords.get(indices.get(0));
+        for (int index : indices) {
+            String word = words.get(index);
+            String sortedWord = sortedWords.get(index);
+            if (group.equals(sortedWord)) {
+                anagrams.add(word);
+                continue;
+            }
+            groupedAnagrams.add(anagrams);
+            anagrams = new ArrayList<>(Arrays.asList(word));
+            group = sortedWord;
+        }
+        groupedAnagrams.add(anagrams);
+        return groupedAnagrams;
     }
 }
