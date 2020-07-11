e096196c8cca12451c4ed8da4b22cf5264199db5
kamal <kamalbctg@gmail.com>
2020-07-11 10:12:59 +0600

    update

--- src/main/java/org/algorithom/BalancedBrackets/Solution.java
+++ src/main/java/org/algorithom/BalancedBrackets/Solution.java
@@ -7,20 +7,20 @@ import java.util.Map;
 
 public class Solution {
     public static boolean balancedBrackets(String str) {
-        String openingBrackets = "({[";
-        String closingBrackets = "]})";
+        String openings = "({[";
+        String closings = "]})";
         Map<Character, Character> closingMatches = new HashMap<>();
         closingMatches.put(')', '(');
         closingMatches.put('}', '{');
         closingMatches.put(']', '[');
         List<Character> stack = new ArrayList<>();
         for (int i = 0; i < str.length(); i++) {
-            char letter = str.charAt(i);
-            if (openingBrackets.indexOf(letter) != -1) {
-                stack.add(letter);
+            char c = str.charAt(i);
+            if (openings.indexOf(c) != -1) {
+                stack.add(c);
             } else {
-                if (stack.size() == 0) return false;  // no opening bracket
-                if (stack.get(stack.size() - 1) == closingMatches.get(letter)) {  // last element closing Matches
+                if (stack.size() == 0) return false;  // no opening bracket in stack
+                if (stack.get(stack.size() - 1) == closingMatches.get(c)) {  // last element closing Matches
                     stack.remove(stack.size() - 1);
                 } else {
                     return false;
